51597731d9c9f1a6e9b5b1dd9bc6f10dd0e53a3e
Yashas Ambati <ysa4idea@gmail.com>
2019-02-10 18:21:45 -0500

    added vis stuff

---
+++ main/graph.py
@@ -0,0 +1,31 @@
+from tbainfo import tbarequests
+from db import dbtools
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+
+db = dbtools("2018Scouting", "frc900", "frc900")
+competition_id = db.get_competition_id("'District Champs'")
+tba = tbarequests('jQusM2aYtJLHXv3vxhDcPpIWzaxjMga5beNRWOarv6wdRwTF63vNpIsLYVANvCWE')
+teams = tba.get_teams('2018nccmp')
+
+scaling_ability = []
+switch_ability = []
+win_p = []
+
+for team in teams:
+    team_id = db.get_team_id(team)
+    matches_team_ids = db.get_matches_team_id(str(team_id), str(competition_id))
+    scale = np.mean(db.get_metric(matches_team_ids, "'Scale'"))
+    scaling_ability.append(scale)
+    switch = np.mean(db.get_metric(matches_team_ids, "'Switch'"))
+    switch_ability.append(switch)
+    win_p.append(tba.get_wins('2018nccmp', team))
+    print('processing... ' + str((teams.index(team)/len(teams))*100) + '%')
+
+plt.scatter(scaling_ability, win_p)
+plt.scatter(switch_ability, win_p)
+plt.show()
+
+
+
--- tbainfo.py
+++ tbainfo.py
@@ -7,21 +7,40 @@ class tbarequests:
         self.headers = {'X-TBA-Auth-Key': key}
 
     def get_teams(self, comp_id):
-        teams = requests.get('https://www.thebluealliance.com/api/v3/event/' + comp_id + '/teams/keys', headers=headers)
+        teams = requests.get('https://www.thebluealliance.com/api/v3/event/' + comp_id + '/teams/keys', headers=self.headers)
         return teams.json()
 
     def get_team_matches(self, comp_id, team):
-        matches = requests.get('https://www.thebluealliance.com/api/v3/team/' + team + '/event/' + comp_id + '/matches/keys', headers=headers)
+        matches = requests.get('https://www.thebluealliance.com/api/v3/team/' + team + '/event/' + comp_id + '/matches/keys', headers=self.headers)
         return matches.json()
 
     def get_all_matches(self, comp_id):
-        matches = requests.get('https://www.thebluealliance.com/api/v3/event/' + comp_id + '/matches/keys', headers=headers)
+        matches = requests.get('https://www.thebluealliance.com/api/v3/event/' + comp_id + '/matches/keys', headers=self.headers)
         return matches.json()
 
     def get_match_score(self, match_id, alliance):
-        data = requests.get('https://www.thebluealliance.com/api/v3/match/' + match_id + '/simple', headers=headers)
+        data = requests.get('https://www.thebluealliance.com/api/v3/match/' + match_id + '/simple', headers=self.headers)
         return data.json()['alliances'][alliance]['score']
 
     def get_match_teams(self, match_id):
-        data = requests.get('https://www.thebluealliance.com/api/v3/match/' + match_id + '/simple', headers=headers)
+        data = requests.get('https://www.thebluealliance.com/api/v3/match/' + match_id + '/simple', headers=self.headers)
         return [data.json()['alliances']['blue']['team_keys'], data.json()['alliances']['red']['team_keys']]
+
+    def get_winning_alliance(self, match_id):
+        data = requests.get('https://www.thebluealliance.com/api/v3/match/' + match_id + '/simple', headers=self.headers)
+        return data.json()['winning_alliance']
+
+    def get_wins(self, comp_id, team_id):
+        wins = 0
+        team_matches = self.get_team_matches(comp_id, team_id)
+        for i in team_matches:
+            match_teams = self.get_match_teams(i)
+            alliance = ''
+            if match_teams[0].count(team_id) == 1:
+                alliance = 'blue'
+            else:
+                alliance = 'red'
+            winning_alliance = self.get_winning_alliance(i)
+            if winning_alliance == alliance:
+                wins += 1
+        return wins/len(team_matches)
